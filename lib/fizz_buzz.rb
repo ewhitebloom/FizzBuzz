a45f946ad441d2c31b722d343079b6c290838027
ewhitebloom <ewhitebloom@gmail.com>
2014-03-14 12:13:12 -0400

    cleaned

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,2 +1,1 @@
-# YOUR CODE HERE
-(1..100).each { |number| if number % 3 == 0 && number % 5 == 0; puts 'FizzBuzz'; elsif number%3 == 0;puts 'Fizz'; elsif number%5 == 0; puts 'Buzz';else; puts number; end; }
+(1..100).each { |number| if number % 3 == 0 && number % 5 == 0; puts 'FizzBuzz'; elsif number % 3 == 0; puts 'Fizz'; elsif number % 5 == 0; puts 'Buzz'; else; puts number; end; }

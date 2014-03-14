6b595ddecf7013612c656a48ec676fab52b3c2f9
ewhitebloom <ewhitebloom@gmail.com>
2014-03-14 13:30:19 -0400

    method-ified

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,1 +1,7 @@
-(1..100).each { |number| if number % 3 == 0 && number % 5 == 0; puts 'FizzBuzz'; elsif number % 3 == 0; puts 'Fizz'; elsif number % 5 == 0; puts 'Buzz'; else; puts number; end; }
+nums = (1..100)
+
+def fizzbuzz(numbers)
+numbers.each { |number| if number % 3 == 0 && number % 5 == 0; puts 'FizzBuzz'; elsif number % 3 == 0; puts 'Fizz'; elsif number % 5 == 0; puts 'Buzz'; else; puts number; end; }
+end
+
+fizzbuzz(nums)
